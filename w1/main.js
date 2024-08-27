dc451edf3db7ce850a439d20c348c44fb2f1050d
Mary Vang <0722881@my.scccd.edu>
2024-08-27 02:30:01 -0400

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+console.log("hello from inside the main.js file")
+
+//let myVar;
+
+const myVar = 42;
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType);
+console.log(`myVarType ${myVarType}`)
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`this line will run`);
+     } else {
+        console.log(`will 13 this one run?`);
+     }
+}
+
+runNow();
+runNow();
