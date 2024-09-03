06776418b492f911bfee55791f571f269472e352
Mary Vang <0722881@my.scccd.edu>
2024-09-03 02:12:04 -0400

    quotes, operators and primitives

---
+++ w3/main.js
@@ -0,0 +1,24 @@
+const myName = "Mary Vang";
+const myAge = 143;
+
+const jsIsWeird = myName + myAge
+
+console.log(jsIsWeird)
+console.log(typeof jsIsWeird)
+console.log(typeof myAge)
+
+
+//what is order of precedence
+//write an example
+
+//const results =
+//console.log(results)
+
+const amIhungry = true;
+console.log(typeof amIhungry);
+
+let myVar;
+console.log(typeof myVar);
+
+const myHeading = document.querySelector("h1");
+console.log(typeof myHeading)
